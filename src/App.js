01e28cdcb209a43219c243bd0d0d1b5e3ed6c81b
Caner Demir <dmrcnr@gmail.com>
2023-06-25 00:29:47 +0300

    Animal project started

--- src/App.js
+++ src/App.js
@@ -1,9 +1,37 @@
+import { useState } from "react";
+import AnimalShow from "./AnimalShow";
+
+function getRandomAnimal() {
+  const animals = ["bird", "cat", "cow", "dog", "gator", "horse"];
+
+  return animals[Math.floor(Math.random() * animals.length)];
+}
+
 function App() {
+  const [animals, setAnimals] = useState([]);
+
+  const handleClick = () => {
+    // push modifies a piece of state!
+    // animals.push(getRandomAnimal());
+    const randomAnimal = getRandomAnimal();
+    if (!animals.includes(randomAnimal)) {
+      setAnimals([...animals, randomAnimal]);
+    }
+  };
+
+  const renderedAnimals = animals.map((animal, i) => {
+    return <AnimalShow type={animal} key={i} />;
+  });
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Animal App</h1>
-      </header>
+    <div>
+      <h1>Animal App</h1>
+      <div className="container">
+        <button type="button" name="addAnimalBtn" onClick={handleClick}>
+          Add animal
+        </button>
+        <div>{renderedAnimals}</div>
+      </div>
     </div>
   );
 }
